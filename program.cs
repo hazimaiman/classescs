d47e19dac6a16d5318ad40953be27a510f06ac68
hazimaiman <79648084+hazimaiman@users.noreply.github.com>
2023-01-11 10:42:56 +0800

    Create program.cs

---
+++ program.cs
@@ -0,0 +1,25 @@
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+
+namespace MyFirstProgram
+    {
+        class Program
+        {
+        static void Main(string[] args)
+        {
+            //class = 
+
+            Messages.Hello();
+            Messages.Waiting();
+
+                Console.ReadKey();  
+        }
+        
+    }
+    }
+
+
+
+
+
+   
