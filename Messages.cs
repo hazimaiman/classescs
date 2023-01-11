a9919d0d1e8021fe58d9d9434a9be79e15bee650
hazimaiman <79648084+hazimaiman@users.noreply.github.com>
2023-01-11 10:43:20 +0800

    Create Messages.cs

---
+++ Messages.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstProgram
+{
+    class Messages
+    {
+    public static void Hello()
+    {
+        Console.WriteLine("Hello!");
+    }
+    public static void Waiting()
+    {
+        Console.WriteLine(" i wait");
+    }
+    public static void Bye()
+    {
+        Console.WriteLine(" BYE! ");
+    }
+    }
+   
+}
